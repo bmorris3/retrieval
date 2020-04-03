ba3f705a4255d108741cf9d5788d137523df84a0
Brett Morris <brettmorris21@gmail.com>
2020-04-03 14:13:35 +0200

    Adding comments, varying threshold

--- example/abc.py
+++ example/abc.py
@@ -18,30 +18,43 @@ def distance(theta):
     return np.sum((example_spectrum[:, 1] - model)**2 /
                   example_spectrum[:, 2]**2) / example_spectrum.shape[0]
 
+
 init_temp = 1500
+n_steps = 1000
 
-distance_chain = [distance([init_temp])]
-temperature_chain = [init_temp]
+thresholds = [1.5, 1.1, 1.0]
 
-n_steps = 5000
+for threshold in thresholds:
+    # Create chains for the distance and temperature
+    distance_chain = [distance([init_temp])]
+    temperature_chain = [init_temp]
 
-threshold = 1
-i = 0
-total_steps = 1
+    # Set some indices
+    i = 0
+    total_steps = 1
 
-while len(temperature_chain) < n_steps:
-    total_steps += 1
-    trial_temp = temperature_chain[i] + 10 * np.random.randn()
-    trial_dist = distance([trial_temp])
+    # Until the chain is the correct number of steps...
+    while len(temperature_chain) < n_steps:
+        # Generate a trial temperature
+        total_steps += 1
+        trial_temp = temperature_chain[i] + 10 * np.random.randn()
 
-    if trial_dist < threshold:
-        i += 1
-        temperature_chain.append(trial_temp)
-        distance_chain.append(trial_dist)
+        # Measure the distance between the trial step and observations
+        trial_dist = distance([trial_temp])
 
-acceptance_rate = i / total_steps
-print("acceptance rate = ", acceptance_rate)
+        # If trial step has distance less than some threshold...
+        if trial_dist < threshold:
+            # Accept the step, add values to the chain
+            i += 1
+            temperature_chain.append(trial_temp)
+            distance_chain.append(trial_dist)
 
-plt.hist(temperature_chain)
+    # Compute the acceptance rate:
+    acceptance_rate = i / total_steps
+    print(f"h = {threshold}, acceptance rate = {acceptance_rate}")
+
+    plt.hist(temperature_chain, histtype='step', lw=2,
+             label=f'h = {threshold}')
+plt.legend()
 plt.xlabel('Temperature [K]')
 plt.show()
